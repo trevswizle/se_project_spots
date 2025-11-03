86581284233c9a76c8bf3afbb3ce74aae8f45c16
Trevor <trevswizle@gmail.com>
2025-11-02 19:20:17 -0700

    add the the js and transition

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,39 @@ const profileModalClsBtn = document.querySelector('#edit-profile-modal .modal__c
 const newPostModalBtn = document.querySelector('.profile__add-btn');
 const newPostModal = document.querySelector('#new-post-modal');
 const newPostModalClsBtn = document.querySelector('#new-post-modal .modal__close-btn');
-
-
+const profileNameEl = document.querySelector('.profile__name');
+const profileRoleEl = document.querySelector('.profile__role');
+const profileNameInput = document.querySelector('#profile-name-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
+const modalFormName = document.querySelector('.modal__form');
 
 modalProfileBtn.addEventListener('click', () => {
+    const newName = profileNameEl.textContent;
+    const newDescript = profileRoleEl.textContent;
+
+    profileNameInput.value = newName;
+    profileDescriptionInput.value = newDescript;
+
     profileModal.classList.remove('modal_is-closed');
     profileModal.classList.add('modal_is-open');
 });
 
-profileModalClsBtn.addEventListener('click', function() {
+modalFormName.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const updatedName = profileNameInput.value;
+    const updatedDesc = profileDescriptionInput.value;
+
+    profileNameEl.textContent = updatedName;
+    profileRoleEl.textContent = updatedDesc;
+
     profileModal.classList.remove('modal_is-open');
     profileModal.classList.add('modal_is-closed');
+});
+
+profileModalClsBtn.addEventListener('click', function() {
+  profileModal.classList.remove('modal_is-open');
+  profileModal.classList.add('modal_is-closed');
 });
 
 newPostModalBtn.addEventListener('click', () => {
